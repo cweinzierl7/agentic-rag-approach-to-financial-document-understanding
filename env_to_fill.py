59ae8dd856bb5448ffaf653e92a434eee78ad7aa
Caroline Weinzierl <carowein1@icloud.com>
2026-02-04 10:59:22 +0100

    env_to_fill

---
+++ env_to_fill.py
@@ -0,0 +1,73 @@
+# Environment Configuration File
+# This file contains configuration settings for the application.
+# Uncomment and set the values as needed
+# And change the file name to .env
+
+
+
+# PDF P A R S E  C O N F I G U R A T I O N
+# LlamaParse
+# PDF_PARSE_API_KEY = 
+
+
+# D A T A B A S E  C O N F I G U R A T I O N
+
+# Vector Database 
+
+# WEAVIATE_URL = 
+# WEAVIATE_API = 
+
+
+# Vector Search Configuration
+# VECTOR_DIMENSION = 1536
+# MAX_SEARCH_RESULTS = 10
+
+# Graph Database
+# Neo4j Configuration
+# NEO4J_URI = 
+# NEO4J_USER = 
+# NEO4J_PASSWORD = 
+
+
+
+# ZEP Configuration
+
+# ZEP_API_KEY = 
+
+
+
+# L L M  C O N F I G U R A T I O N
+
+# LLM Provider Configuration
+LLM_PROVIDER = openai
+
+# Base URL for the LLM provider
+# LLM_BASE_URL = 
+
+# API Key for the LLM provider
+# LLM_API_KEY = 
+
+# LLM_CHOICE = 
+
+# LLM_CHOICE_KG = 
+
+# for Langchain table summary
+# OPENAI_API_KEY = 
+
+# LLM you want to use for the agents
+
+
+# E M B E D D I N G S
+
+# Embedding Provider Configuration
+EMBEDDING_PROVIDER = openai
+
+# Base URL for the Embedding provider
+# EMBEDDING_BASE_URL = 
+
+# API Key for the Embedding provider
+# EMBEDDING_API_KEY = 
+
+# Embedding model to use
+# EMBEDDING_MODEL = 
+
